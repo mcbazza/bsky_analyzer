82e3104206a50f8415030cebd791227e05375391
Bazza <40249989+mcbazza@users.noreply.github.com>
2024-11-18 21:38:38 +0000

    Update secrets.py
    clarify instructions

--- secrets.py
+++ secrets.py
@@ -1,5 +1,18 @@
 # Replace with your Bluesky handle and password
-# Use an app password, not your legit one!!
+# You can get this by looking at the address of your 'profile' page
+#
+# e.g. for the account https://bsky.app/profile/bsky.app the accountname is bsky.app
+# handle="bsky.app"
+#
+# For accounts using a custom domain, it's the same.
+# e.g. for https://bsky.app/profile/atproto.com the accountname is atproto.com
+# handle="atproto.com"
 
+# Your username here
 handle="your-handle-here-without-the-at"
+
+# Use an app password, not your legit one!!
+# To create one, visit: https://bsky.app/settings/app-passwords
+# And create one.
+# Then place it into this string
 password="hunter2"
